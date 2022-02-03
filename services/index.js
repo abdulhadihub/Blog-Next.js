306df61f9167036697df5d9fc23ec43740c4b6f6
Abdul Hadi <88116803+LocTrain@users.noreply.github.com>
2022-02-03 17:24:46 +0500

    added Post Details Page

--- services/index.js
+++ services/index.js
@@ -1,3 +1,4 @@
+import { constants } from 'buffer';
 import { request, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
@@ -37,6 +38,41 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: {slug: $slug}){
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+              content{
+                raw
+              }
+            }
+        }
+      
+    `;
+
+  const result = await request(graphqlAPI, query, { slug })
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
   query GetPostDetails(){
@@ -56,7 +92,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
   query GetPostDetails($slug: String!, $categories: [String!]){
     posts(
@@ -72,6 +108,20 @@ export const getSimilarPosts = async () => {
   }
   }
   `
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, { categories, slug })
   return result.posts;
+}
+
+export const getCategories = async () => {
+  const query = gql`
+  query GetCategories {
+    categories {
+      name
+      slug
+    }
+  }
+  `
+  const result = await request(graphqlAPI, query)
+  return result.categories;
+
 }
